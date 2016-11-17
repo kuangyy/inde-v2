58efc8aaf1aead04fc666c4666c7a02ca129cb6a
kuangye <ky.kys.cn@hotmail.com>
2016-11-17 22:13:14 +0800

    aaa i wanna a bigger screen

--- src/main/java/cn/kykys/index/business/game/DramaBusiness.java
+++ src/main/java/cn/kykys/index/business/game/DramaBusiness.java
@@ -5,13 +5,18 @@ import cn.kykys.index.data.wechat.DramaPlayModelMapper;
 import cn.kykys.index.data.wechat.NodeModelMapper;
 import cn.kykys.index.ibusiness.game.IDrama;
 import cn.kykys.index.model.enumeration.NodeTypeEnum;
+import cn.kykys.index.model.ext.ChooseModel;
 import cn.kykys.index.model.ext.NodeDetail;
 import cn.kykys.index.model.wechat.DramaModel;
 import cn.kykys.index.model.wechat.NodeModelWithBLOBs;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by kuangye on 2016/11/17.
@@ -37,19 +42,29 @@ public class DramaBusiness implements IDrama {
     }
 
 
-
-    NodeDetail getFirstNodeByDramaId(Integer dramaId){
+    NodeDetail getFirstNodeByDramaId(Integer dramaId) {
 
         HashMap map = new HashMap();
-        map.put("dramaId",dramaId);
+        map.put("dramaId", dramaId);
         map.put("type", NodeTypeEnum.START.getStatus());
 
         NodeModelWithBLOBs nodeModelWithBLOBs = nodeModelMapper.selectByType(map);
 
+        if (StringUtils.hasText(nodeModelWithBLOBs.getChoices())) {
+
+            List<ChooseModel> chooseModelList = JSON.parseObject(nodeModelWithBLOBs.getChoices(), new TypeReference<List<ChooseModel>>() {
+            });
+
+
+
+
+
+        }
+
 
     }
 
-    NodeDetail getNextNodeByDramaId(Integer dramaId){
+    NodeDetail getNextNodeByDramaId(Integer dramaId) {
 
     }
 
